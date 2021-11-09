7f8552164f427f85f63b2c756d33914a3907b2c4
daxpedda <daxpedda@gmail.com>
2021-11-09 13:38:06 +0100

    Add another test to make sure everything remains the same

--- tests/safety.rs
+++ tests/safety.rs
@@ -1,12 +1,14 @@
 #[test]
 fn discriminant() {
-    use core::mem::{discriminant, transmute};
+    use core::mem::{discriminant, size_of, size_of_val, transmute};
 
     enum Test {
         A,
         B,
         C,
     }
+
+    assert_eq!(size_of::<isize>(), size_of_val(&discriminant(&Test::A)));
 
     assert_eq!(0_isize, unsafe { transmute(discriminant(&Test::A)) });
     assert_eq!(1_isize, unsafe { transmute(discriminant(&Test::B)) });
