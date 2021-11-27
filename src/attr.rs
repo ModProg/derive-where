70195adb092bfe6d746c50ac6fc80430aa3e8083
ModProg <dev@modprog.de>
2021-11-27 15:24:36 +0100

    disable zeroize import without feature

--- src/attr.rs
+++ src/attr.rs
@@ -12,4 +12,5 @@ pub use field::FieldAttr;
 pub use item::{DeriveTrait, DeriveWhere, ItemAttr};
 use skip::Skip;
 pub use variant::VariantAttr;
+#[cfg(feature = "zeroize")]
 use zeroize_fqs::ZeroizeFqs;
