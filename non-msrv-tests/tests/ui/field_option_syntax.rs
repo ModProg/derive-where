0bfec38105c90f4a8b3dd6366ab276c4ab854186
daxpedda <daxpedda@gmail.com>
2021-12-01 02:09:30 +0100

    Add tests for empty field or variant attributes

--- non-msrv-tests/tests/ui/field_option_syntax.rs
+++ non-msrv-tests/tests/ui/field_option_syntax.rs
@@ -1,8 +1,23 @@
 #[derive(derive_where::DeriveWhere)]
 #[derive_where(Default; T)]
-enum Test<T> {
-    #[derive_where = "default"]
-    A(T),
+struct Test1<T>(#[derive_where] T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where(Default; T)]
+struct Test2<T>(#[derive_where = "default"] T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where(Default; T)]
+enum Test3<T> {
+	#[derive_where]
+	A(T),
+}
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where(Default; T)]
+enum Test4<T> {
+	#[derive_where = "default"]
+	A(T),
 }
 
 fn main() {}
