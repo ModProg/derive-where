1c72dca2d16bdcc6c8cf916d5eeaf3a40e12f85d
daxpedda <daxpedda@gmail.com>
2021-12-01 02:09:59 +0100

    Add tests for invalid struct field attributes

--- non-msrv-tests/tests/ui/field_option.rs
+++ non-msrv-tests/tests/ui/field_option.rs
@@ -1,8 +1,12 @@
 #[derive(derive_where::DeriveWhere)]
-#[derive_where(Default; T)]
-enum Test<T> {
-    #[derive_where(option)]
-    A(T),
+#[derive_where(Clone; T)]
+struct Test1<T>(#[derive_where(option)] T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where(Clone; T)]
+enum Test2<T> {
+	#[derive_where(option)]
+	A(T),
 }
 
 fn main() {}
