16f7a7a286d416af6d533a2597385d700a7abeae
daxpedda <daxpedda@gmail.com>
2021-12-01 01:42:29 +0100

    Allow trailing `,`

--- non-msrv-tests/tests/ui/zeroize/comma.rs
+++ non-msrv-tests/tests/ui/zeroize/comma.rs
@@ -1,9 +1,5 @@
 #[derive(derive_where::DeriveWhere)]
-#[derive_where(Clone,; T)]
-struct Test1<T>(T);
-
-#[derive(derive_where::DeriveWhere)]
 #[derive_where(Clone,,)]
-struct Test2<T>(T);
+struct Test<T>(T);
 
 fn main() {}
