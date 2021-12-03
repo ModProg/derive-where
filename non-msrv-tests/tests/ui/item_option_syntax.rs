0a94b12a9059e0dfbf3b3cbf2d932e4862ba7fd9
daxpedda <daxpedda@gmail.com>
2021-12-03 13:25:42 +0100

    Improve checks and add tests
    - Disable some test files that contain no tests on the cargo-level
    - Add and improve comments around `ItemAttr`, `DeriveWhere`, `DeriveTrait` and `Skip` parsing

--- non-msrv-tests/tests/ui/item_option_syntax.rs
+++ non-msrv-tests/tests/ui/item_option_syntax.rs
@@ -1,9 +1,29 @@
 #[derive(derive_where::DeriveWhere)]
-#[derive_where]
 struct Test1<T>(T);
 
 #[derive(derive_where::DeriveWhere)]
-#[derive_where(Debug = "option"; T)]
+#[derive_where]
 struct Test2<T>(T);
+
+#[derive(derive_where::DeriveWhere)]
+// Rust itself already fails to parse this and will provide a separate error message.
+#[derive_where = invalid]
+struct Test3<T>(T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where = "invalid"]
+struct Test4<T>(T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where()]
+struct Test5<T>(T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where(Debug = "option"; T)]
+struct Test6<T>(T);
+
+#[derive(derive_where::DeriveWhere)]
+#[derive_where(Debug(); T)]
+struct Test7<T>(T);
 
 fn main() {}
--- src/attr/item.rs
+++ src/attr/item.rs
@@ -1,14 +1,15 @@
-//! Attribute parsing for items.
+//! [`Attribute`] parsing for items.
 
 use core::ops::Deref;
+use std::borrow::Cow;
 
 use proc_macro2::Span;
 use syn::{
 	parse::{discouraged::Speculative, Parse, ParseStream},
 	punctuated::Punctuated,
 	spanned::Spanned,
-	Attribute, Data, Meta, NestedMeta, Path, PredicateType, Result, Token, TraitBound, Type,
-	TypeParamBound, WherePredicate,
+	Attribute, Data, Ident, Meta, NestedMeta, Path, PredicateType, Result, Token, TraitBound, Type,
+	TypeParamBound, TypePath, WhereClause, WherePredicate,
 };
 
 use crate::{util, Error, Item, Skip, Trait, TraitImpl, DERIVE_WHERE};
@@ -16,7 +17,7 @@ use crate::{util, Error, Item, Skip, Trait, TraitImpl, DERIVE_WHERE};
 /// Attributes on item.
 #[derive(Default)]
 pub struct ItemAttr {
-	/// [`Trait`](crate::Trait)s to skip all fields for.
+	/// [`Trait`]s to skip all fields for.
 	pub skip_inner: Skip,
 	/// [`DeriveWhere`]s on this item.
 	pub derive_wheres: Vec<DeriveWhere>,
@@ -26,16 +27,16 @@ impl ItemAttr {
 	/// Create [`ItemAttr`] from [`Attribute`]s.
 	pub fn from_attrs(span: Span, data: &Data, attrs: &[Attribute]) -> Result<Self> {
 		let mut self_ = ItemAttr::default();
-		let mut skip_inner = None;
+		let mut skip_inners = Vec::new();
 
 		for attr in attrs {
 			if attr.path.is_ident(DERIVE_WHERE) {
 				if let Ok(meta) = attr.parse_meta() {
 					if let Meta::List(list) = meta {
 						match list.nested.len() {
-							// Don't allow empty list.
+							// Don't allow an empty list.
 							0 => return Err(Error::empty(list.span())),
-							// Allow `skip_inner` if list only has one item.
+							// Check for `skip_inner` if list only has one item.
 							1 => match list
 								.nested
 								.into_iter()
@@ -47,47 +48,52 @@ impl ItemAttr {
 										// Don't allow `skip_inner` on the item level for enums.
 										if let Data::Enum(_) = data {
 											return Err(Error::option_enum_skip_inner(meta.span()));
-										} else {
-											skip_inner = Some(meta);
 										}
-									} else {
-										self_.derive_wheres.push(DeriveWhere::from_attr(attr)?);
+
+										// Don't parse `Skip` yet, because it needs access to all
+										// `DeriveWhere`s.
+										skip_inners.push(meta);
+									}
+									// The list can have one item but stil not be the `skip_inner`
+									// attribute, continue with parsing `DeriveWhere`.
+									else {
+										self_
+											.derive_wheres
+											.push(DeriveWhere::from_attr(span, data, attr)?);
 									}
 								}
 								nested_meta => {
 									return Err(Error::option_syntax(nested_meta.span()))
 								}
 							},
-							_ => self_.derive_wheres.push(DeriveWhere::from_attr(attr)?),
+							_ => self_
+								.derive_wheres
+								.push(DeriveWhere::from_attr(span, data, attr)?),
 						}
 					} else {
 						return Err(Error::option_syntax(meta.span()));
 					}
-				} else {
-					self_.derive_wheres.push(DeriveWhere::from_attr(attr)?)
+				}
+				// Anything that can't be parsed by `Meta`, is because `A, B; C` isn't valid syntax.
+				else {
+					self_
+						.derive_wheres
+						.push(DeriveWhere::from_attr(span, data, attr)?)
 				}
 			}
 		}
 
-		// Delay parsing of `skip_inner` to get access to all traits to be implemented.
-		if let Some(meta) = skip_inner {
-			self_
-				.skip_inner
-				.add_attribute(&self_.derive_wheres, None, &meta)?;
-		}
-
+		// Check that we specified at least one `#[derive_where(..)]` with traits.
 		if self_.derive_wheres.is_empty() {
 			return Err(Error::none(span));
 		}
 
-		if let Data::Union(_) = data {
-			for derive_where in &self_.derive_wheres {
-				for trait_ in &derive_where.traits {
-					if !trait_.supports_union() {
-						return Err(Error::union(span));
-					}
-				}
-			}
+		// Delayed parsing of `skip_inner` to get access to all traits to be
+		// implemented.
+		for meta in skip_inners {
+			self_
+				.skip_inner
+				.add_attribute(&self_.derive_wheres, None, &meta)?;
 		}
 
 		Ok(self_)
@@ -104,62 +110,69 @@ pub struct DeriveWhere {
 	pub generics: Vec<Generic>,
 }
 
-impl Parse for DeriveWhere {
-	/// Parse the macro input, this should either be:
-	/// - Comma separated traits.
-	/// - Comma separated traits `;` Comma separated generics.
-	fn parse(input: ParseStream) -> Result<Self> {
-		let mut traits = Vec::new();
-		let mut generics = Vec::new();
-
-		// Start parsing traits.
-		while !input.is_empty() {
-			traits.push(DeriveTrait::parse(input)?);
-
-			if !input.is_empty() {
-				let mut fork = input.fork();
-				let mut delimiter_found = None;
-
-				match <Token![,]>::parse(&fork) {
-					Ok(_) => {
-						input.advance_to(&fork);
-					}
-					Err(error) => {
-						delimiter_found = Some(error.span());
-						fork = input.fork();
-					}
-				}
-
-				if <Token![;]>::parse(&fork).is_ok() {
-					input.advance_to(&fork);
-
-					// If we found a semi-colon, start parsing generics.
-					if !input.is_empty() {
-						generics = Punctuated::<Generic, Token![,]>::parse_terminated(input)?
-							.into_iter()
-							.collect();
-					}
-				} else if let Some(span) = delimiter_found {
-					return Err(Error::derive_where_delimiter(span));
-				}
-			}
-		}
-
-		Ok(Self {
-			span: Span::call_site(),
-			generics,
-			traits,
-		})
-	}
-}
-
 impl DeriveWhere {
 	/// Create [`DeriveWhere`] from [`Attribute`].
-	fn from_attr(attr: &Attribute) -> Result<Self> {
-		let span = attr.span();
-		let mut derive_where: DeriveWhere = attr.parse_args()?;
-		derive_where.span = span;
-		Ok(derive_where)
+	fn from_attr(span: Span, data: &Data, attr: &Attribute) -> Result<Self> {
+		attr.parse_args_with(|input: ParseStream| {
+			// Parse the attribute input, this should either be:
+			// - Comma separated traits.
+			// - Comma separated traits `;` Comma separated generics.
+
+			let mut traits = Vec::new();
+			let mut generics = Vec::new();
+
+			// Check for an empty list is already done in `ItemAttr::from_attrs`.
+			while !input.is_empty() {
+				// Start with parsing a trait.
+				// Not checking for duplicates here, because these should produce a Rust error
+				// anyway.
+				traits.push(DeriveTrait::from_stream(span, data, input)?);
+
+				if !input.is_empty() {
+					let mut fork = input.fork();
+
+					// Track `Span` of whatever was found instead of a delimiter. We parse the `,`
+					// first because it's allowed to be followed by a `;`.
+					let no_delimiter_found = match <Token![,]>::parse(&fork) {
+						Ok(_) => {
+							input.advance_to(&fork);
+							None
+						}
+						Err(error) => {
+							// Reset the fork if we didn't find a `,`.
+							fork = input.fork();
+							Some(error.span())
+						}
+					};
+
+					if <Token![;]>::parse(&fork).is_ok() {
+						input.advance_to(&fork);
+
+						// If we found a semi-colon, start parsing generics.
+						if !input.is_empty() {
+							// `parse_teminated` parses everything left, which should end the
+							// while-loop.
+							// Not checking for duplicates here, as even Rust doesn't give a warning
+							// for those: `where T: Clone, T: Clone` produces no error or warning.
+							generics = Punctuated::<Generic, Token![,]>::parse_terminated(input)?
+								.into_iter()
+								.collect();
+						}
+					}
+					// We are here because the input isn't empty, but we also found no delimiter,
+					// something unexpected is here instead.
+					else if let Some(span) = no_delimiter_found {
+						return Err(Error::derive_where_delimiter(span));
+					}
+				}
+			}
+
+			Ok(Self {
+				span: attr.span(),
+				generics,
+				traits,
+			})
+		})
 	}
 
 	/// Returns selected [`DeriveTrait`] if present.
@@ -168,12 +181,67 @@ impl DeriveWhere {
 			.iter()
 			.find(|derive_trait| ***derive_trait == trait_)
 	}
+
+	/// Returns `true` if any [`CustomBound`](Generic::CustomBound) is present.
+	pub fn any_custom_bound(&self) -> bool {
+		self.generics.iter().any(|generic| match generic {
+			Generic::CustomBound(_) => true,
+			Generic::NoBound(_) => false,
+		})
+	}
+
+	/// Returns `true` if the given generic type parameter if present.
+	pub fn has_type_param(&self, type_param: &Ident) -> bool {
+		self.generics.iter().any(|generic| match generic {
+			Generic::NoBound(Type::Path(TypePath { qself: None, path })) => {
+				if let Some(ident) = path.get_ident() {
+					ident == type_param
+				} else {
+					false
+				}
+			}
+			_ => false,
+		})
+	}
+
+	/// Create [`WhereClause`] for the given parameters.
+	pub fn where_clause(
+		&self,
+		where_clause: &mut Option<Cow<WhereClause>>,
+		trait_: &DeriveTrait,
+		item: &Item,
+	) {
+		// Only create a where clause if required
+		if !self.generics.is_empty() {
+			// We use the existing where clause or create a new one if required.
+			let where_clause = where_clause.get_or_insert(Cow::Owned(WhereClause {
+				where_token: <Token![where]>::default(),
+				predicates: Punctuated::default(),
+			}));
+
+			// Insert bounds into the `where` clause.
+			for generic in &self.generics {
+				where_clause
+					.to_mut()
+					.predicates
+					.push(WherePredicate::Type(match generic {
+						Generic::CustomBound(type_bound) => type_bound.clone(),
+						Generic::NoBound(path) => PredicateType {
+							lifetimes: None,
+							bounded_ty: path.clone(),
+							colon_token: <Token![:]>::default(),
+							bounds: trait_.where_bounds(item),
+						},
+					}));
+			}
+		}
+	}
 }
 
 /// Holds a single generic [type](Type) or [type with bound](PredicateType).
 pub enum Generic {
 	/// Generic type with custom [specified bounds](PredicateType).
-	CoustomBound(PredicateType),
+	CustomBound(PredicateType),
 	/// Generic [type](Type) which will be bound to the [`DeriveTrait`].
 	NoBound(Type),
 }
@@ -183,13 +251,14 @@ impl Parse for Generic {
 		let fork = input.fork();
 
 		// Try to parse input as a `WherePredicate`. The problem is, both expressions
-		// start with a Type, so this is the easiest way of differentiating them.
+		// start with a Type, so starting with the `WherePredicate` is the easiest way
+		// of differentiating them.
 		if let Ok(where_predicate) = WherePredicate::parse(&fork) {
-			// Advance input as if `WherePredicate` was parsed on it.
 			input.advance_to(&fork);
 
+			// Don't allow lifetimes, as it doesn't make sense in the context.
 			if let WherePredicate::Type(path) = where_predicate {
-				Ok(Generic::CoustomBound(path))
+				Ok(Generic::CustomBound(path))
 			} else {
 				Err(Error::generic(where_predicate.span()))
 			}
@@ -232,29 +301,6 @@ pub enum DeriveTrait {
 	},
 }
 
-impl Parse for DeriveTrait {
-	fn parse(input: ParseStream) -> Result<Self> {
-		match Meta::parse(input) {
-			Ok(meta) => {
-				let trait_ = Trait::from_path(meta.path())?;
-
-				match meta {
-					Meta::Path(_) => Ok(trait_.default_derive_trait()),
-					Meta::List(list) => {
-						if list.nested.is_empty() {
-							return Err(Error::option_empty(list.span()));
-						}
-
-						trait_.parse_derive_trait(list)
-					}
-					Meta::NameValue(name_value) => Err(Error::option_syntax(name_value.span())),
-				}
-			}
-			Err(error) => Err(Error::trait_syntax(error.span())),
-		}
-	}
-}
-
 impl Deref for DeriveTrait {
 	type Target = Trait;
 
@@ -278,6 +324,36 @@ impl Deref for DeriveTrait {
 }
 
 impl DeriveTrait {
+	/// Create [`DeriveTrait`] from [`ParseStream`].
+	fn from_stream(span: Span, data: &Data, input: ParseStream) -> Result<Self> {
+		match Meta::parse(input) {
+			Ok(meta) => {
+				let trait_ = Trait::from_path(meta.path())?;
+
+				if let Data::Union(_) = data {
+					// Make sure this `Trait` supports unions.
+					if !trait_.supports_union() {
+						return Err(Error::union(span));
+					}
+				}
+
+				match meta {
+					Meta::Path(_) => Ok(trait_.default_derive_trait()),
+					Meta::List(list) => {
+						if list.nested.is_empty() {
+							return Err(Error::option_empty(list.span()));
+						}
+
+						// This will return an error if no options are supported.
+						trait_.parse_derive_trait(list)
+					}
+					Meta::NameValue(name_value) => Err(Error::option_syntax(name_value.span())),
+				}
+			}
+			Err(error) => Err(Error::trait_syntax(error.span())),
+		}
+	}
+
 	/// Returns fully qualified path for this trait.
 	pub fn path(&self) -> Path {
 		use DeriveTrait::*;
@@ -306,7 +382,7 @@ impl DeriveTrait {
 	}
 
 	/// Returns where-clause bounds for the trait in respect of the item type.
-	pub fn where_bounds(&self, data: &Item) -> Punctuated<TypeParamBound, Token![+]> {
+	fn where_bounds(&self, data: &Item) -> Punctuated<TypeParamBound, Token![+]> {
 		let mut list = Punctuated::new();
 
 		list.push(TypeParamBound::Trait(TraitBound {
