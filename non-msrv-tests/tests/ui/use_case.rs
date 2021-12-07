b5a31b23c075bef15c827febc1881e991d6c7eab
daxpedda <daxpedda@gmail.com>
2021-12-07 21:09:04 +0100

    Add UI tests for empty items

--- non-msrv-tests/tests/ui/use_case.rs
+++ non-msrv-tests/tests/ui/use_case.rs
@@ -2,6 +2,22 @@ use derive_where::DeriveWhere;
 
 #[derive(DeriveWhere)]
 #[derive_where(Clone)]
+struct StructEmpty {}
+
+#[derive(DeriveWhere)]
+#[derive_where(Clone)]
+struct TupleEmpty();
+
+#[derive(DeriveWhere)]
+#[derive_where(Clone)]
+struct Unit;
+
+#[derive(DeriveWhere)]
+#[derive_where(Clone)]
+struct UnionEmpty {}
+
+#[derive(DeriveWhere)]
+#[derive_where(Clone)]
 struct StructNone(u8);
 
 #[derive(DeriveWhere)]
