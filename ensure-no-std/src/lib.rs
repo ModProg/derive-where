b568af33d2d1ab1b95015bcaf66ccb7f3848ade7
daxpedda <daxpedda@gmail.com>
2021-12-07 11:36:08 +0100

    Tiny improvement in ensure-no-std

--- ensure-no-std/src/lib.rs
+++ ensure-no-std/src/lib.rs
@@ -5,7 +5,9 @@ extern crate zeroize_ as zeroize;
 
 use core::marker::PhantomData;
 
-#[derive(derive_where::DeriveWhere)]
+use derive_where::DeriveWhere;
+
+#[derive(DeriveWhere)]
 #[derive_where(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "zeroize", derive_where(Zeroize))]
 pub struct Test<T>(#[cfg_attr(feature = "zeroize", derive_where(skip(Zeroize)))] PhantomData<T>);
