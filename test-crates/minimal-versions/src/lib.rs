c4738a96d006639b942136fcc5dd06a37dad1e2b
dAxpeDDa <daxpedda@gmail.com>
2022-03-07 12:21:34 +0100

    Test minimal versions
    Move testing crates into `test-crates`

---
+++ test-crates/minimal-versions/src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg(feature = "zeroize")]
+extern crate zeroize_ as zeroize;
+
+use core::marker::PhantomData;
+
+use derive_where::derive_where;
+
+#[derive_where(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "zeroize", derive_where(Zeroize))]
+pub struct Test<T>(PhantomData<T>);
