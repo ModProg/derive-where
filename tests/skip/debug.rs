23f439b30d0fcc39b4387acc568a5c58caf68f1c
dAxpeDDa <daxpedda@gmail.com>
2023-03-19 16:25:22 +0100

    Add struct enum variant tests

--- tests/skip/debug.rs
+++ tests/skip/debug.rs
@@ -3,7 +3,7 @@ use derive_where::derive_where;
 use crate::util::{AssertDebug, Wrapper};
 
 #[test]
-fn all() {
+fn struct_all() {
 	#[derive_where(Debug)]
 	#[derive_where(skip_inner)]
 	struct Test<T> {
@@ -18,7 +18,7 @@ fn all() {
 }
 
 #[test]
-fn partial() {
+fn struct_partial() {
 	#[derive_where(Debug)]
 	struct Test<T> {
 		#[derive_where(skip)]
@@ -34,4 +34,40 @@ fn partial() {
 	let _ = AssertDebug(&test_1);
 
 	assert_eq!(format!("{:?}", test_1), "Test { b: 42, .. }");
+}
+
+#[test]
+fn variant_all() {
+	#[derive_where(Debug)]
+	enum Test<T> {
+		#[derive_where(skip_inner)]
+		A { a: Wrapper<T> },
+	}
+
+	let test_1 = Test::A { a: 42.into() };
+
+	let _ = AssertDebug(&test_1);
+
+	assert_eq!(format!("{:?}", test_1), "A { .. }");
+}
+
+#[test]
+fn variant_partial() {
+	#[derive_where(Debug)]
+	enum Test<T> {
+		A {
+			#[derive_where(skip)]
+			a: Wrapper<T>,
+			b: Wrapper<T>,
+		},
+	}
+
+	let test_1 = Test::A {
+		a: 42.into(),
+		b: 42.into(),
+	};
+
+	let _ = AssertDebug(&test_1);
+
+	assert_eq!(format!("{:?}", test_1), "A { b: 42, .. }");
 }
