ea7fe4522a78eba8d094a79ae34c65077c647cad
daxpedda <daxpedda@gmail.com>
2023-12-14 12:41:09 +0100

    Test for `clippy::non_canonical_clone_impl`

---
+++ tests/misc.rs
@@ -0,0 +1,8 @@
+use std::marker::PhantomData;
+
+use derive_where::derive_where;
+
+/// Test for `clippy::non_canonical_clone_impl`, which should not be triggered
+/// because of `#[automatically_derived]`.
+#[derive_where(Clone, Copy; T: Copy)]
+struct NonCanonicalCloneImpl<T>(PhantomData<T>);
