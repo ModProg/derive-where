b3c8b97d3e7149c5ad393d82641d647b1e325cdf
daxpedda <daxpedda@gmail.com>
2025-05-16 17:08:39 +0200

    Test minimal version without `serde(crate = "...")`

--- test-crates/minimal-versions/src/lib.rs
+++ test-crates/minimal-versions/src/lib.rs
@@ -7,6 +7,5 @@ use derive_where::derive_where;
 
 #[derive_where(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive_where(Deserialize, Serialize))]
-#[cfg_attr(feature = "serde", serde(crate = "serde_"))]
 #[cfg_attr(feature = "zeroize", derive_where(Zeroize))]
 pub struct Test<T>(PhantomData<T>);
