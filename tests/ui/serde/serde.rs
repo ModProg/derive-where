8808e7bddccb0a205818c92c5fb2f4b91e31af68
daxpedda <daxpedda@gmail.com>
2025-06-20 13:15:06 +0200

    Add test

--- tests/ui/serde/serde.rs
+++ tests/ui/serde/serde.rs
@@ -2,6 +2,10 @@ use std::marker::PhantomData;
 
 use derive_where::derive_where;
 
+#[derive_where(Clone)]
+#[serde(crate = "serde_")]
+struct ValidSerde<T>(PhantomData<T>);
+
 #[derive_where(Deserialize)]
 #[serde(bound = "")]
 struct ConflictingBound<T>(PhantomData<T>);
